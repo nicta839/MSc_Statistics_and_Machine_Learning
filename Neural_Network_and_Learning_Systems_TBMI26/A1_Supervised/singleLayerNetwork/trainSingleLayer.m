2a13a96f36ced66d0f130c4df18a4f6b875a5d75
nicta839 <70749265+nicta839@users.noreply.github.com>
2021-02-06 11:25:40 +0100

    Advanced Data Mining first commit

--- Neural_Network_and_Learning_Systems_TBMI26/A1_Supervised/singleLayerNetwork/trainSingleLayer.m
+++ Neural_Network_and_Learning_Systems_TBMI26/A1_Supervised/singleLayerNetwork/trainSingleLayer.m
@@ -27,7 +27,7 @@ ErrTest(1)  = sum(sum((YTest  - DTest ).^2)) / NTest;
 for n = 1:numIterations
     % Add your own code here
     % Transpose for matrix multiplication
-    grad_w = (2/NTrain) * ((Wout * XTrain) - DTrain)* XTrain'; % Normalize to number of train cases
+    grad_w = (2/NTrain) * ((Wout * XTrain) - DTrain) * XTrain'; % Normalize to number of train cases
 
     % Take a learning step
     Wout = Wout - learningRate * grad_w;
